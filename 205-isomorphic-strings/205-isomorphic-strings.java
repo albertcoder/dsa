ee65a7acf8d7fbd3096e99056771132e702cd307
Albert Massey <eralbert9191@gmail.com>
2021-12-27 17:37:27 -0800

    Time: 12 ms (38.62%), Space: 38.9 MB (67.66%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s == null || t == null) return false;
+        HashMap<Character, Character> sHash = new HashMap<>();
+        HashMap<Character, Character> tHash = new HashMap<>();
+        if(s.length() != t.length()) return false;
+        // if(s.length() == t.length()){
+            for(int i = 0; i < s.length(); i++){
+                char a = s.charAt(i);
+                char b = t.charAt(i);
+                if(!sHash.containsKey(a)){
+                    sHash.put(a, b);
+                } else{
+                    if(sHash.get(a) != b) return false;
+                }
+                if(!tHash.containsKey(b)){
+                    tHash.put(b, a);
+                } else{
+                    if(tHash.get(b) != a) return false;
+                }
+            }
+        // }
+        return true;
+    }
+}
