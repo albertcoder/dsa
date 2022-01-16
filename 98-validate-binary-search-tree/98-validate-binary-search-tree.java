17abb2e8243385fefcc14d136ac6aac8828bdc01
Albert Massey <eralbert9191@gmail.com>
2022-01-15 19:06:45 -0800

    Time: 0 ms (100.00%), Space: 41.3 MB (9.79%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,20 +14,16 @@
  * }
  */
 class Solution {
+    TreeNode prev = null;
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode prev = null;
-        while (root != null || !st.isEmpty()) {
-            while (root != null) {
-                st.push(root);
-                root = root.left;
-            }
-            root = st.pop();
-            if (prev != null &&  prev.val >= root.val) return false;
-            prev = root;
-            root = root.right;
-        }
-        return true;
+        return inorder(root);
+    }
+    private boolean inorder(TreeNode root){
+        if(root == null) return true;
+        
+        if(inorder(root.left) == false) return false;
+        if(prev != null && prev.val >= root.val) return false;
+        prev = root;
+        return inorder(root.right);
     }
 }
