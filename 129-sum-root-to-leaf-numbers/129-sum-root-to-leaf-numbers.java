a152b399170a3de9d37ca4efa0eedf852bd20cc4
Albert Massey <eralbert9191@gmail.com>
2022-02-01 23:07:11 -0800

    Time: 0 ms (100.00%), Space: 42 MB (5.26%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.java
@@ -13,34 +13,55 @@
  *     }
  * }
  */
+
 class Solution {
-    class Pair {
-        TreeNode node;
-        int val = 0;
-        public Pair(TreeNode node, int val) {
-            this.node = node;
-            this.val = val;
-        }
-    }
+    int result = 0;
     public int sumNumbers(TreeNode root) {
-        if (root == null) return 0;
-        int result = 0; int sum = 0;
-        Stack<Pair> st = new Stack<>();
-        while(root != null || !st.isEmpty()){
-            while(root != null) {
-                sum = sum * 10 + root.val;
-                st.push(new Pair(root, sum));
-                root = root.left;
-            }
-            Pair p = st.pop();
-            root = p.node;
-            sum = p.val;
-            if (root.left == null && root.right == null) {
-                result += sum;
-            }
-            System.out.println(root.val);
-            root = root.right;
-        }
+        dfs(root, 0);
         return result;
     }
+    public void dfs(TreeNode root, int sum) {
+        //base
+        if (root == null) return;
+        
+        //logic
+        if (root.left == null && root.right == null) {
+            result = result + sum * 10 + root.val;
+        }
+        dfs(root.left, sum * 10 + root.val);
+        dfs(root.right, sum * 10 + root.val);
+    }
 }
+
+
+// class Solution {
+//     class Pair {
+//         TreeNode node;
+//         int val = 0;
+//         public Pair(TreeNode node, int val) {
+//             this.node = node;
+//             this.val = val;
+//         }
+//     }
+//     public int sumNumbers(TreeNode root) {
+//         if (root == null) return 0;
+//         int result = 0; int sum = 0;
+//         Stack<Pair> st = new Stack<>();
+//         while(root != null || !st.isEmpty()){
+//             while(root != null) {
+//                 sum = sum * 10 + root.val;
+//                 st.push(new Pair(root, sum));
+//                 root = root.left;
+//             }
+//             Pair p = st.pop();
+//             root = p.node;
+//             sum = p.val;
+//             if (root.left == null && root.right == null) {
+//                 result += sum;
+//             }
+//             System.out.println(root.val);
+//             root = root.right;
+//         }
+//         return result;
+//     }
+// }
